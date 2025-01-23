a1610514598e5ce5c311c5a7593d8ffdb6c20480
Sirihegde02 <118575770+Sirihegde02@users.noreply.github.com>
2025-01-23 17:15:56 -0500

    [238] [Time Beats: 27.55%] [Memory Beats: 9.02%] - LeetPush

--- 238-Product-of-Array-Except-Self.py
+++ 238-Product-of-Array-Except-Self.py
@@ -14,27 +14,46 @@ class Solution:
         #     output[i] = product
         # return output
 
-        #Using the division operator:
-        #Time complexity: O(n)
-        #Space complexity: O(1) since the output array is excluded from space analysis.
-        n = len(nums)
-        output = [1] * n
-        product = 1
-        zero_flag = 0
-        for num in nums:
-            if num:
-                product *= num
-            else:
-                zero_flag += 1
-        if zero_flag > 1:
-            return [0] * n
+        # #Using the division operator:
+        # #Time complexity: O(n)
+        # #Space complexity: O(1) since the output array is excluded from space analysis.
+        # n = len(nums)
+        # output = [1] * n
+        # product = 1
+        # zero_flag = 0
+        # for num in nums:
+        #     if num:
+        #         product *= num
+        #     else:
+        #         zero_flag += 1
+        # if zero_flag > 1:
+        #     return [0] * n
 
-        for i,c in enumerate(nums):
-            if zero_flag:
-                if c:
-                    output[i] = 0
-                else:
-                    output[i] = product
-            else:
-                output[i] = product // c
+        # for i,c in enumerate(nums):
+        #     if zero_flag:
+        #         if c:
+        #             output[i] = 0
+        #         else:
+        #             output[i] = product
+        #     else:
+        #         output[i] = product // c
+        # return output
+
+        #Best approach:
+        #Using three lists , where two lists store the previous and upcoming number products
+        #Time complexity: O(n)
+        #Space complexity: O(n)
+        n = len(nums)
+        output = [0] * n
+        pref = [0] * n
+        suff = [0] * n
+
+        pref[0] = suff[n - 1] = 1
+        for i in range(1, n):
+            pref[i] = nums[i - 1] * pref[i - 1]
+        #Traversing from n-2 end to -1 beginning:
+        for i in range(n - 2, -1, -1):
+            suff[i] = nums[i + 1] * suff[i + 1]
+        for i in range(n):
+            output[i] = pref[i] * suff[i]
         return output
